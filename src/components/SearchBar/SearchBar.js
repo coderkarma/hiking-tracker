6b593f6f034ba241b36f54e01342498098d2b4d6
Karma Drukpa <karmadrukpa11@gmail.com>
2019-03-31 14:57:43 -0700

    Update SearchBar.js
    removing api keys

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -14,7 +14,7 @@ class SearchBar extends Component {
 	getLocation = e => {
 		// 1600+Amphitheatre+Parkway,+Mountain+View,+CA
 		let address = this.state.value.replace(/\s/g, '+');
-		let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyAVaHMTuOSJrUzFISOWTUtJMiSXLh6BuwM`;
+		
 		console.log(url);
 		let promise = fetch(url).then(response => response.json());
 		// .then(json => json);
@@ -32,7 +32,7 @@ class SearchBar extends Component {
 
 				console.log('handle submit', JSON.stringify(location));
 				let promise = fetch(
-					`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxDistance=10&key=200439239-c0f23da15aa93f591bfc0baf98024eeb`
+					
 				).then(response => response.json());
 				return promise;
 			})
